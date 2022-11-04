a182304b9d6312ecc086cc5afce8e0c2421040d3
Puja Mukherjee <pujam836@gmail.com>
2022-11-03 18:45:47 -0700

    settig up hello books

--- app/routes.py
+++ app/routes.py
@@ -29,26 +29,26 @@ books_bp = Blueprint("books", __name__, url_prefix="/books")
 #     return response_body
 
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-books = [
-    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-] 
+# books = [
+#     Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ] 
 
-@books_bp.route("", methods=["GET"])
-def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_response)
+# @books_bp.route("", methods=["GET"])
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description
+#         })
+#     return jsonify(books_response)
 
