ec2eb589a8d03b972b18904d22de00ac1bbeb4ef
Puja Mukherjee <pujam836@gmail.com>
2022-11-03 20:18:38 -0700

    get all books and get all books with matching book title

--- app/routes.py
+++ app/routes.py
@@ -69,18 +69,18 @@ def handle_books():
 
     return make_response(f"Book {new_book.title} successfully created", 201)
 
-@books_bp.route("", methods=["GET"])
-def get_all_books():
-    if request.method == "GET":
-        books = Book.query.all()
-        books_response = []
-        for book in books:
-            books_response.append({
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            })
-        return jsonify(books_response)
+# @books_bp.route("", methods=["GET"])
+# def get_all_books():
+#     if request.method == "GET":
+#         books = Book.query.all()
+#         books_response = []
+#         for book in books:
+#             books_response.append({
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             })
+#         return jsonify(books_response)
 
 def validate_book(book_id):
     try:
@@ -116,4 +116,34 @@ def update_book(book_id):
     db.session.commit()
 
     return make_response(f"Book #{book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")
+
+@books_bp.route("", methods=["GET"])
+def filter_all_books_with_matching_title():
+
+    # this code replaces the previous query all code
+    title_query = request.args.get("title")
+    if title_query:
+        books = Book.query.filter_by(title=title_query)
+    else:
+        books = Book.query.all()
+    # end of the new code
+
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+
+    return jsonify(books_response)
     
