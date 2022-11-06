d90fea49037155bef84ead90e03da942740aedef
Puja Mukherjee <pujam836@gmail.com>
2022-11-06 15:20:18 -0800

    one to many relationships

--- app/models/book.py
+++ app/models/book.py
@@ -1,16 +1,40 @@
-from app import db
+# from app import db
 
+
+# class Book(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     title = db.Column(db.String)
+#     description = db.Column(db.String)
+
+# # Create an empty dictionary
+# # for each attribute in Book 
+# #   create a key with the attribute's name
+# #   set it to the value of the attribute
+# # return the dictionary
+#     def to_dict(self):
+#         book_as_dict = {}
+#         book_as_dict["id"] = self.id
+#         book_as_dict["title"] = self.title
+#         book_as_dict["description"] = self.description
+
+#         return book_as_dict
+
+#     @classmethod
+#     def from_dict(cls, book_data):
+#         new_book = Book(title=book_data["title"],
+#                     description=book_data["description"])
+#         return new_book
+
+
+from app import db
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    author = db.relationship("Author", back_populates="books")
 
-# Create an empty dictionary
-# for each attribute in Book 
-#   create a key with the attribute's name
-#   set it to the value of the attribute
-# return the dictionary
     def to_dict(self):
         book_as_dict = {}
         book_as_dict["id"] = self.id
@@ -22,5 +46,5 @@ class Book(db.Model):
     @classmethod
     def from_dict(cls, book_data):
         new_book = Book(title=book_data["title"],
-                    description=book_data["description"])
+                        description=book_data["description"])
         return new_book
